46c86147bb63ee9fee1bfec75db0d69db3773fa5
Minako Nakanishi <n135216e@gmail.com>
2019-04-17 17:57:17 +0900

    タイトルの実装、コンポーネントの設置、テキストボックスのバインド準備

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Todo title="Todo List"/>
+  </div>
+</template>
+
+<script>
+  import Todo from './components/Todo.vue'
+
+  export default{
+    name: 'app',
+    components:{
+      Todo
+    }
+  }
+</script>
